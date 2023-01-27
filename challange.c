9a313d28b559982ebef63424cb85224058548237
Rafael Dalepiane <90656891+RafaDalepi@users.noreply.github.com>
2023-01-27 16:56:41 +0000

    Not working

--- challange.c
+++ challange.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+const int wordsQtde = 15 ;
 
 int main(){
-    char repetitions[100];
+    char* repetitions, letters[wordsQtde];    
+    int aparitions = 0 , rest = 0 ;
 
-    printf("Your text: \n");
-    scanf("%s", &repetitions);
+    printf("Your text:\n");
+    fgets(letters, sizeof(letters), stdin);
 
-    printf("%s", repetitions);
+    char * token = strtok(letters, " ");
 
+   while( token != NULL ) {
+        // printf( " %s\n", token ); 
+        token = strtok(NULL, " ");
+        if (token == "the"){
+            aparitions++;
+        }else{
+            rest++;       
+        }
+        printf("%s\n %d - %d\n", token, aparitions, rest);
+    }
 }
+    
